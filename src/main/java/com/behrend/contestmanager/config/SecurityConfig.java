7d4c215cba598ebce03cf5fcbbd942fdec052af8
Kevin Muirhead <kxm5887@psu.edu>
2023-02-04 14:25:48 -0500

    Added SecConfig class

---
+++ src/main/java/com/behrend/contestmanager/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.behrend.contestmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
